a962cc60788fac1fc9301a524be851776774dbec
sjaiswalgit <subhashkjaiswal333@gmail.com>
2024-10-09 23:54:46 +0530

    Preview Scroll Fixed

--- src/pages/Preview.tsx
+++ src/pages/Preview.tsx
@@ -12,7 +12,7 @@ const Preview: React.FC = () => {
   },[])
   
   return (
-    <div style={{height:'100vh',width:'100vw',margin:0,padding:0}} >
+    <div >
       {components.map((component, index) => (
         <PreviewHelper
           key={index}
